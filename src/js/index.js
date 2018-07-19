ca6d2bb34e6df9c7912a3c07983fc49bab8696e5
Elsa Brown <elsambrown@gmail.com>
2018-07-19 07:34:52 -0400

    one arrow moves

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,24 @@ const makeAnchorListeners = () => {
 		.querySelectorAll('.nav-link'));
 	items.forEach(li => {
 		li.addEventListener('click', () => {
-			let target = document.getElementById(`a-${li.id}`);
-			console.log('target: ', target);
-			
+			let target = document.getElementById(`a-${li.id}`);	
 			// target.scrollIntoView({behavior: "smooth", block: "start"});
 		})
+	})
+};
+
+const makeArrowListeners = () => {
+	let arrow1 = document.querySelector('.icon-arrow');
+	arrow1.addEventListener('click', (event) => {
+		console.log('target: ', event.target);
+		event.target.classList.add('icon-arrow--rotate');
 	})
 }
 
 
 const init = () => {
 	makeAnchorListeners();
+	makeArrowListeners();
 };
 
 init();
